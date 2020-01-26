01e2bf4d6e0dd2f3443b571831d3fe6f564544af
ktleary <kevin@ktleary.com>
2020-01-26 17:19:22 -0500

    Added additional tests

--- test/entries.spec.js
+++ test/entries.spec.js
@@ -1,34 +1,34 @@
 "use strict";
 
 const assert = require("chai").assert;
-const { splitCalenderEntry } = require("../funcs");
+const { splitFirstRest } = require("../funcs");
 
 describe("Date parsing", () => {
 	it("returns undefined when no argument is supplied.", () => {
 		const entry = undefined;
 		const delim = "/";
-		const splitEntry = splitCalenderEntry(entry, delim);
+		const splitEntry = splitFirstRest(entry, delim);
 		
 		assert.equal(splitEntry, undefined);
 	});
 	it("return two strings when a string with one delimiter is supplied.", () => {
 		const entry = "abcde/12345";
 		const delim = "/";
-		const splitEntry = splitCalenderEntry(entry, delim);
+		const splitEntry = splitFirstRest(entry, delim);
 		
 		assert.equal(splitEntry.length, 2);
 	});
 	it("return two strings when a string with two delimiter is supplied.", () => {
 		const entry = "abcde/12345/1a2b3c";
 		const delim = "/";
-		const splitEntry = splitCalenderEntry(entry, delim);
+		const splitEntry = splitFirstRest(entry, delim);
 		
 		assert.equal(splitEntry.length, 2);
 	});
 	it("returns undefined if no event is supplied after the delimiter.", () => {
 		const entry = "abcde/";
 		const delim = "/";
-		const splitEntry = splitCalenderEntry(entry, delim);
+		const splitEntry = splitFirstRest(entry, delim);
 		
 		assert.equal(splitEntry, undefined);
 	});

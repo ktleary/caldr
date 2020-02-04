ef15b227cc03175d001a0fcf7dcbc02b88805cde
ktleary <kevin@ktleary.com>
2020-02-04 12:37:31 -0500

    updated userSettings.setUserSettings to check for files and directories and create stubs

--- index.js
+++ index.js
@@ -4,33 +4,29 @@ const fs = require('fs');
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const Configstore = require('configstore');
-
 const files = require('./lib/files');
+const config = require('./lib/configs');
 
 const systemSettings = require('./systemSettings');
 const userSettings = require('./lib/userSettings');
+const entries = require('./lib/entries');
 const constants = require('./constants');
 
-clear();
+// clear();
 
 console.log(
 	chalk.yellow(figlet.textSync('caldr', { horizontalLayout: 'full' }))
 );
 
-const dbExists = () => {
-	const files = fs.readdirSync(`${systemSettings.locations.defaultDbDir}`);
-	return files.includes(`${systemSettings.locations.dbFile}`);
-};
-
 const run = async () => {
-	if (!process.argv[2])
+	const entry = process.argv[2];
+	if (!entry) {
 		return console.log(`Error: ${constants.errors.NOENTRY}`);
-	const baseEntry = process.argv[2];
-	const _dbExists = dbExists();
-
-	(!_dbExists || baseEntry === constants.commands.RESETCONFIG) &&
-		userSettings.setUserSettings();
+	} else if (entry === constants.commands.RESETCONFIG || !config.checkcfg()) {
+		return userSettings.setUserSettings();
+	} else {
+		return entries.processEntry(baseEntry);
+	}
 };
 
 run();

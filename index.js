ec37a96f3cc28be8376c0f485bfed17d392d7063
ktleary <kevin@ktleary.com>
2020-05-05 08:13:28 -0400

    removed cruft

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const fs = require('fs');
 const chalk = require('chalk');
 const clear = require('clear');
@@ -13,18 +11,18 @@ const constants = require('./constants');
 clear();
 
 console.log(
-	chalk.yellow(figlet.textSync('caldr', { horizontalLayout: 'full' }))
+  chalk.yellow(figlet.textSync('caldr', { horizontalLayout: 'full' }))
 );
 
 const run = async () => {
-	const entry = process.argv[2];
-	if (!entry) {
-		return console.log(`Error: ${constants.errors.NOENTRY}`);
-	} else if (entry === constants.commands.RESETCONFIG || !checkcfg()) {
-		return userSettings.setUserSettings();
-	} else {
-		return entries.processEntry(entry);
-	}
+  const entry = process.argv[2];
+  if (!entry) {
+    return console.log(`Error: ${constants.errors.NOENTRY}`);
+  } else if (entry === constants.commands.RESETCONFIG || !checkcfg()) {
+    return userSettings.setUserSettings();
+  } else {
+    return entries.processEntry(entry);
+  }
 };
 
 run();
